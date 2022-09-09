c58f72e9e23788723b2609cdc1a5ee779a3ececc
Valentina Gabarron <valentinagabarron06@gmail.com>
2022-09-09 13:47:07 -0300

    Ejercicio: grupo 2

--- src/com/alkemy/Grupo2/Main.java
+++ src/com/alkemy/Grupo2/Main.java
@@ -1,8 +1,18 @@
 package com.alkemy.Grupo2;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        for (int i = 0; i <8 ; i++) {
+            for (int j = 0; j <8 ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            if (i % 2 == 0) {
+                System.out.print(" ");
+            }
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        }
     }
+
 }
--- src/com/alkemy/Grupo2/README.md
+++ src/com/alkemy/Grupo2/README.md
@@ -19,3 +19,4 @@ System.out.println();
      * * * * * * * *
     * * * * * * * *
      * * * * * * * *
+by Daiana y Valentina
